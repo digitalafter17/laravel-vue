3067b56a1b1910f5a1863bd8a2e48c987d12018d
After Seventeen <101615755+digitalafter17@users.noreply.github.com>
2022-04-20 09:07:45 +0700

    Update broadcast.vue
    20/04/2022

--- resources/js/views/admin/broadcast.vue
+++ resources/js/views/admin/broadcast.vue
@@ -4,13 +4,28 @@
         <div class="card-header py-3">
           <h6 class="m-0 font-weight-bold text-primary">Creat Broadcast</h6>
         </div>
+        <button class="btn btn-primary py-3 align="right"">
+          <h1>Add Broadcast</h1>
+        </button>
+        <button class="btn btn-primary py-3 align="left"">
+          <h1>View Email Broadcast</h1>
+        </button>
         <div class="card-body">
           <ul>id</ul>
           <ul>nama</ul>
           <ul>email</ul>
           <ul>pesan</ul>
         </div>
-        
+        <table>
+          <th>
+            <td>
+              <tr>{{panggil id}}</tr>
+              <tr>{{panggil nama}}</tr>
+              <tr>{{panggil email}}</tr>
+              <tr>{{panggil new pesan}}</tr>
+            </td>
+          </th>
+        </table>
         <div class="content wrap mb-4">
           <div class="card-content">
             
