92e843ef30874da08e93ddbb8b5433d2ccf4afd4
After Seventeen <101615755+digitalafter17@users.noreply.github.com>
2022-04-20 09:58:35 +0700

    Update dashboard.vue
    nur-hidayatul-akbar

--- resources/js/views/admin/dashboard.vue
+++ resources/js/views/admin/dashboard.vue
@@ -12,17 +12,8 @@
             <div class="card-body">
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                  <div class="
-                          text-xs
-                          font-weight-bold
-                          text-primary text-uppercase
-                          mb-1
-                        ">
-                    Anggota
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    40,000
-                  </div>
+                  <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Anggota</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">40,000</div>
                 </div>
                 <div class="col-auto">
                   <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -37,17 +28,8 @@
             <div class="card-body">
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                  <div class="
-                          text-xs
-                          font-weight-bold
-                          text-success text-uppercase
-                          mb-1
-                        ">
-                    Transaksi
-                  </div>
-                  <div class="h5 mb-0 font-weight-bold text-gray-800">
-                    215,000
-                  </div>
+                  <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Transaksi</div>
+                  <div class="h5 mb-0 font-weight-bold text-gray-800">215,000</div>
                 </div>
                 <div class="col-auto">
                   <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -62,14 +44,10 @@
             <div class="card-body">
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                    History
-                  </div>
+                  <div class="text-xs font-weight-bold text-info text-uppercase mb-1">History</div>
                   <div class="row no-gutters align-items-center">
                     <div class="col-auto">
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                        50
-                      </div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50</div>
                     </div>
                     <div class="col">
                       <div class="progress progress-sm mr-2">
@@ -90,14 +68,7 @@
             <div class="card-body">
               <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                  <div class="
-                          text-xs
-                          font-weight-bold
-                          text-warning text-uppercase
-                          mb-1
-                        ">
-                    Email Broadcast
-                  </div>
+                  <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Email Broadcast</div>
                   <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                 </div>
                 <div class="col-auto">
