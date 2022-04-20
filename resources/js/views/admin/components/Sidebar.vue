798ce7e15039623d3b919deb64e43f2067e103a9
After Seventeen <101615755+digitalafter17@users.noreply.github.com>
2022-04-20 00:25:45 -0400

    Update Sidebar.vue
    nur-hidayatul-akbar

--- resources/js/views/admin/components/Sidebar.vue
+++ resources/js/views/admin/components/Sidebar.vue
@@ -2,20 +2,16 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
         <!-- Sidebar - Brand -->
         <router-link class="sidebar-brand d-flex align-items-center justify-content-center" to="/">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-laugh-wink"></i>
-            </div>
+            <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
             <div class="sidebar-brand-text mx-3">Actionpay<sup></sup></div>
         </router-link>
-
         <!-- Divider -->
+        
         <hr class="sidebar-divider my-0" />
-
+        
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <router-link class="nav-link" to="/admin">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></router-link>
+            <router-link class="nav-link" to="/admin"><i class="fas fa-fw fa-tachometer-alt"></i><span>Dashboard</span></router-link>
         </li>
 
         <!-- Divider -->
@@ -26,38 +22,29 @@
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                aria-expanded="true" aria-controls="collapseTwo">
-                <i class="fas fa-fw fa-cog"></i>
-                <span>List Components</span>
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i><span>List Components</span>
             </a>
 
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom List:</h6>
+                <div class="bg-white py-2 collapse-inner rounded"><h6 class="collapse-header">Custom List:</h6>
                     <router-link class="collapse-item" to="/admin/anggota">List Anggota</router-link>
                     <router-link class="collapse-item" to="/admin/transaksi">List Transaksi</router-link>
                 </div>
             </div>
-
         </li>
 
         <!-- Divider -->
         <hr class="sidebar-divider" />
-
         <!-- Heading -->
         <div class="sidebar-heading">Broadcast</div>
-
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                aria-expanded="true" aria-controls="collapsePages">
-                <i class="fas fa-fw fa-folder"></i>
-                <span>Broadcast Email</span>
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i><span>Broadcast Email</span>
             </a>
             <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">Custom Broadcast</h6>
+                <div class="bg-white py-2 collapse-inner rounded"><h6 class="collapse-header">Custom Broadcast:</h6>
                     <router-link class="collapse-item" to="/admin/history">List History</router-link>
                     <router-link class="collapse-item" to="/admin/broadcast">New Broadcast</router-link>
                 </div>
