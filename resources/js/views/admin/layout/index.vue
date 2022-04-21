f7c9186721579ea340a5370adf8cdd80bcba806b
After Seventeen <101615755+digitalafter17@users.noreply.github.com>
2022-04-21 13:26:41 +0700

    Update index.vue
    nur-hidayatul-akbar

--- resources/js/views/admin/layout/index.vue
+++ resources/js/views/admin/layout/index.vue
@@ -25,7 +25,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-          <span>Copyright &copy; 2022-<script>document.write(new Date().getFullYear())</script> Actionpay</span>
+            <span>Copyright &copy; 2022 - <script>document.write(new Date().getFullYear())</script>Actionpay</span>
           </div>
         </div>
       </footer>
