98d5fdb2e52c256f3ff20d230fbcee9f9aa9006b
After Seventeen <101615755+digitalafter17@users.noreply.github.com>
2022-04-21 08:38:20 +0700

    Update anggota.vue
    nur-hidayatul-akbar

--- resources/js/views/admin/anggota.vue
+++ resources/js/views/admin/anggota.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <!-- Page Heading -->
-        <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Data Tables Anggota</h6>
